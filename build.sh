c69e8dfe3f116d433b9bd68b1f85fb748ac79f11
Jordy Versmissen <jordy.versmissen@sqills.com>
2021-04-23 15:27:37 +0200

    Only set JAVA_PATH if it's not set before

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
-JAVA_PATH=/Library/Java/JavaVirtualMachines/amazon-corretto-11.jdk/Contents/Home/bin/
+if [ -z $JAVA_PATH ]; then
+  JAVA_PATH=$HOME/Library/Java/JavaVirtualMachines/corretto-11.0.10/Contents/Home/bin
+fi
+
+echo "JAVA_PATH = $JAVA_PATH"
 
 $JAVA_PATH/javac src/pocsnakeyaml/PocScriptEngineFactory.java
 $JAVA_PATH/jar -cvf poc.jar -C src/ .
